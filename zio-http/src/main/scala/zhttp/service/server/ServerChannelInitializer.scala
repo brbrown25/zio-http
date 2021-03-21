2118b52cd9054d1eed6b4d816b8eb2a963b5d950
Tushar Mathur <tusharmath@gmail.com>
2021-03-21 21:38:25 +0530

    fix(server): handle very large request size

--- zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
+++ zio-http/src/main/scala/zhttp/service/server/ServerChannelInitializer.scala
@@ -7,12 +7,12 @@ import zhttp.service.{HTTP_REQUEST_HANDLER, OBJECT_AGGREGATOR, SERVER_CODEC_HAND
  * Initializes the netty channel with default handlers
  */
 @JSharable
-final case class ServerChannelInitializer(httpH: JChannelHandler) extends JChannelInitializer[JChannel] {
+final case class ServerChannelInitializer(httpH: JChannelHandler, maxSize: Int) extends JChannelInitializer[JChannel] {
   override def initChannel(channel: JChannel): Unit = {
     channel
       .pipeline()
       .addLast(SERVER_CODEC_HANDLER, new JHttpServerCodec)
-      .addLast(OBJECT_AGGREGATOR, new JHttpObjectAggregator(Int.MaxValue))
+      .addLast(OBJECT_AGGREGATOR, new JHttpObjectAggregator(maxSize))
       .addLast(HTTP_REQUEST_HANDLER, httpH)
     ()
   }
